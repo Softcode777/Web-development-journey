8ace83b17c2a8ee5d1eef630973f052bba6239e8
Hp@DESKTOP-88ITRTJ <156195113+Softcode777@users.noreply.github.com>
2025-07-21 08:30:17 +0500

    First commit

---
+++ Javascript Journey/Arthimetaic Operators/Index.js
@@ -0,0 +1,15 @@
+console.log("Hello I am Conditional tutorial")
+
+let age = '15'
+
+if (age >=18 ) {
+console.log ('You can drive ')
+}
+
+else  (age < 18 ) {
+    console.log ('you cannot drive ')
+}
+
+if (age>=60 ) {
+    console.log ('your driving license is expire ')
+}
